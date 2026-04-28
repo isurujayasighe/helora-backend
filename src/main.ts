ecd76c4acd4ad2efe9044198dfca5e42cc70f58d
AzureAD\IsuruwanJayasinghe <isuruwan.jayasinghe@covalentworld.com>
2026-04-28 14:48:31 +0530

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-      "https://your-frontend.up.railway.app",
+      "https://cloudflare-workers-autoconfig-helora-frontend.isuruwanjayasinghe.workers.dev",
     ],
     credentials: true,
   });
